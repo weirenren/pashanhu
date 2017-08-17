de3ff6f02bc7d84cb467e22386d4a26251ecca38
superchao <306917331@qq.com>
2017-08-18 02:39:12 +0800

    youhua

--- model/appUserInfo.js
+++ model/appUserInfo.js
@@ -8,7 +8,7 @@ var schema = new mongoose.Schema({
     deviceid: String,
     date: Date, // 激活日期
     qrcode: String,
-    vipcode: {type:String, default: ''} // 0代码没有购买会员
+    vipcode: {type:String, default: 'null'} // 0代码没有购买会员
 });
 var User = mongoose.model('app_user_info', schema);
 module.exports = User;
