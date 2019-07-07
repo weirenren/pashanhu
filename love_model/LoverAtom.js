f9b0bc28bc76b2226f4baad3a2274ec12bd43970
Johnweichao <johnweichao@didichuxing.com>
2019-07-07 13:00:15 +0800

    add token

--- love_model/LoverAtom.js
+++ love_model/LoverAtom.js
@@ -8,8 +8,8 @@ var schema = new mongoose.Schema({
     local_address: {type: String, default: ""},
     local_gender: {type: Number, default: 0},// 0: boy; 1: girl
     local_hometown: {type: String, default: ""}, // 入职地点
-    local_age: {type: Number, default: 0},
-    local_height: {type: Number, default: 0.0},
+    local_age: {type: String, default: 0},
+    local_height: {type: String, default: 0.0},
     local_edu: {type: String, default: ""},
     local_workyears: {type: String, default: ""},
     local_job: {type: String, default: ""},
@@ -18,8 +18,8 @@ var schema = new mongoose.Schema({
     local_imgurl_list: {type: String, default: ''}, // 个人照片
 
     condi_gender: {type: Number, default: 1},
-    condi_age: {type: Number, default: 0},
-    condi_height: {type: Number, default: 0},
+    condi_age: {type: String, default: 0},
+    condi_height: {type: String, default: 0},
     condi_edu: {type: String, default: ""},
     condi_hometown: {type: String, default: ""},
     condi_otherinfor: {type: String, default: ""},
